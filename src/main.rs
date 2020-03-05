2476158bf3a79d4446fc4f86074d29667776367f
Carlos Fdez. Llamas <hello@sirikon.me>
2020-03-05 20:42:10 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate reqwest;
+extern crate scraper;
+
+use scraper::{Html, Selector};
+
+fn foo() -> Result<(), reqwest::Error> {
+    let text = reqwest::blocking::get("https://sirikon.me")?.text()?;
+    let document = Html::parse_document(&text);
+    let selector = Selector::parse("title").unwrap();
+
+    for element in document.select(&selector) {
+        println!("{}", element.inner_html());
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    foo().unwrap();
 }
